e14914e263316969d2ecc65ad7d5b0b62f3820be
RockoGilbert <evolve2122@gmail.com>
2021-06-23 20:31:54 -0500

    Wrote a program that prints a puzzle followed bty a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
+#include <stdio.h>
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-	int main(void)
+ *  * main - Prints "Programming is like building a multilingual
+ *   *                puzzle, followed by a new line.
+ *    *
+ *     * Return: Always 0.
+ *      */
+int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
+
 			return (0);
+}
+
