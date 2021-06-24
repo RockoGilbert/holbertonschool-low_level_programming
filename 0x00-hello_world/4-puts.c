4f70237b00e72f3fdad63c73c9ee5f7eecf2dac9
RockoGilbert <evolve2122@gmail.com>
2021-06-23 21:16:53 -0500

    Added a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * Return: Always 0 (Success)
- */
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
 int main (void)
 {
-	puts ("Programming is like building a multlingual puzzle");
-	return (0);
+puts ("Programming is like building a multlingual puzzle");
+return (0);
 }
