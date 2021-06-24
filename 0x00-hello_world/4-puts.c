84166d04f5813d37d3982f3154abf3398205ecd1
RockoGilbert <evolve2122@gmail.com>
2021-06-23 20:25:14 -0500

    Wrote a program that prints a puzzle followed bty a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 /**
  *  * main - Entry point
  *   *
@@ -6,6 +5,5 @@
  *     */
 	int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
-}
