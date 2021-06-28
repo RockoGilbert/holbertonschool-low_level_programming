176855361c2c6c9491f3e3057abf3c34d77ed1d8
RockoGilbert <evolve2122@gmail.com>
2021-06-28 14:22:38 -0500

    Betty

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,19 @@
 #include "holberton.h"
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Return: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
-	putchar('H');
-	putchar('o');
-	putchar('l');
-	putchar('b');
-	putchar('e');
-	putchar('r');
-	putchar('t');
-	putchar('o');
-	putchar('n');
-	putchar('\n');
-
-	return (0);
-}
+       	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+return (0);i
