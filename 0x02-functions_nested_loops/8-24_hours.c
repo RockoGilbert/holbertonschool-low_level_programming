bb3a693395390caf457df0b790366090fd6c461c
RockoGilbert <evolve2122@gmail.com>
2021-06-29 14:32:02 -0500

    loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,20 +2,20 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
 	char c;
 	int n;
 for (n = 48; n < 58; n++)
-	{
+{
 	putchar(n);
-		}
-for (c = 'a'; c  <= 'f'; c++)
+			}
+for (c = 'a'; c <= 'f'; c++)
 {
 	putchar(c);
-		}
+			}
 
 	putchar('\n');
 
