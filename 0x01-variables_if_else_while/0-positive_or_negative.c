d8cfafd9d54718fa943336a04b093e5b1b7cf452
RockoGilbert <evolve2122@gmail.com>
2021-06-28 22:10:24 -0500

    Changed the description

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ * betty style doc for function main goes there
+*/
 int main(void)
 {
 int n;
