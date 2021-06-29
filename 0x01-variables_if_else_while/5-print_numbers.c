993f0e2fa22591d25986f09a9fdce9b1ec246b66
RockoGilbert <evolve2122@gmail.com>
2021-06-28 22:32:07 -0500

    Push

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 	int n;
-		for (n = 0; n <= 9; n++)
-			printf("%i", n);
-			putchar('\n');
+for (n = 0; n <= 9; n++)
 
-		return (0);
+printf("%i", n;
+	putchar('\n');
+	return (0);
 }
