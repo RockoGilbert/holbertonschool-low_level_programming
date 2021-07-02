7e0d7a3e1c373395f32940b124375e0acc5816c4
RockoGilbert <evolve2122@gmail.com>
2021-07-01 20:09:16 -0500

    yes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,21 @@
  */
 void print_numbers(void)
 {
-	int x;
+	int x = 0;
 
-	for (x = 0; x <= 9; x++)
+	for (; x <= 9; x++)
+	{
+
+	if (x == 2 || x == 4)
+	{
+		continue;
+	}
+
+	else
 	{
 		_putchar(x + '0');
-		}
-
-	_putchar('\n');
+	}
+	}
+		_putchar('\n');
 }
+
