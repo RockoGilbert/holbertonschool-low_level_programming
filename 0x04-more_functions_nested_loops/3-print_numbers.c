83b15406ec8e34bd1da8ec7c16665f8dfea825a6
RockoGilbert <evolve2122@gmail.com>
2021-07-01 20:44:52 -0500

    Yes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,24 +3,14 @@
 /**
  * print_numbers - Print the numbers since 0 up to 9
  * Return: The numbers since 0 up to 9
- */
+*/
 void print_numbers(void)
 {
-	int x = 0;
+	int x;
 
-	for (; x <= 9; x++)
-	{
-
-	if (x == 2 || x == 4)
-	{
-		continue;
-	}
-
-	else
+	for (x = 0; x <= 9; x++)
 	{
 		_putchar(x + '0');
 	}
-	}
 		_putchar('\n');
 }
-
