6e817b072843f9fd2700d5c972f665b1fbc8c870
RockoGilbert <evolve2122@gmail.com>
2021-07-06 18:52:53 -0500

    Prints string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _puts - print a string followed by a newline
+ * @str: the string to print
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+		_putchar('\n');
+		return;
+}	
