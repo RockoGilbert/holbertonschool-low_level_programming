87cd4599fe0c16433e031a144f9098cdc2b86af8
RockoGilbert <evolve2122@gmail.com>
2021-07-06 22:24:39 -0500

    Prints every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,12 +12,13 @@ void puts2(char *str)
 	i = 0;
 	x = 0;
 	while (str[i++])
-		{
-			x++;
-		}
+	{
+		x++;
+	}
+
 	for (i = 0; i < x; i += 2)
-		{
-			_putchar(*(str + i));
-		}
-			_putchar('\n');
+	{
+		_putchar(*(str + i));
+	}
+		_putchar('\n');
 }
