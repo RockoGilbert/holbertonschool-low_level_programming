7458035bcda96daf86aade7f1338e8754e5c0c12
RockoGilbert <evolve2122@gmail.com>
2021-07-06 23:01:53 -0500

    prints the second half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,8 +12,7 @@ void puts_half(char *str)
 	i = 0;
 	while (str[i])
 		i++;
-		j = i / 2;
-
+	j = i / 2;
 	if (i % 2)
 		j += 1;
 	while (j < i)
@@ -21,5 +20,5 @@ void puts_half(char *str)
 		_putchar(str[j]);
 		j++;
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
