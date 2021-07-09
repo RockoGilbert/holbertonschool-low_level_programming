85f089d823ed5025a137e068adf4fe66d4c8c585
RockoGilbert <evolve2122@gmail.com>
2021-07-08 22:05:50 -0500

    Compared strings in a function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - copy a string
+ * @dest:char
+ * @src:char
+ * @nint:char
+ * Return:char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int a = 0, b = 0;
+
+	while (src[b])
+	{
+		b++;
+	}
+
+	while (a < n && src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+
+	}
+
+	return (dest);
+}
+
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - compare 2 strings
+ * @s1: The destination value
+ * @s2: The source value
+ * Return: char value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (((*s1 != '\0') && (*s2 != '\0'))	&& (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
+
+}
