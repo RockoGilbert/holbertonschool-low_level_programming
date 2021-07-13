b6c926ef6c54ec05fb178241421ea1fa22a25ed8
RockoGilbert <evolve2122@gmail.com>
2021-07-12 23:04:41 -0500

    sytnax error fixed

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,12 +8,17 @@
 
 void _print_rev_recursion(char *s)
 {
-if (*s)
-{
-	_print_rev_recursion(s + 1);
-		_putchar(*s);
+	if (*s == '\0')
+	{
+		return;
+	}
 
-}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+
+
 }
 
 
