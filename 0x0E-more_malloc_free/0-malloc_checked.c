f89ed3cd026a6a0bed5b66c9169ccf9c77c69dce
RockoGilbert <evolve2122@gmail.com>
2021-07-23 22:24:34 -0500

    undefined refere3nce

--- 0x0E-more_malloc_free/0-malloc_checked.c
+++ 0x0E-more_malloc_free/0-malloc_checked.c
@@ -5,19 +5,15 @@
 /**
  * malloc_checked - Allocates memory
  * @b: the size to allocate
- *
- *
+ * Return: Nothing
  */
 
 void *malloc_checked(unsigned int b)
-
 {
-	void *p;
-
-	p = malloc(b);
-
-	if (p == NULL)
-		exit(98);
-
-	return (p);
+void *p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+return (p);
 }
