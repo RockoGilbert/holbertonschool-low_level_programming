2957aa67ee685856bf62547017e74e95e0af195d
RockoGilbert <evolve2122@gmail.com>
2021-07-26 12:38:43 -0500

    Changed the pointer for age

--- 0x0F-structures_typedef/dog.h
+++ 0x0F-structures_typedef/dog.h
@@ -12,17 +12,10 @@
 typedef struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 
-};
-
-/**
- * dog _t - Typedef for dog structure
- */
-typedefstruct dog dog_t;
-
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
