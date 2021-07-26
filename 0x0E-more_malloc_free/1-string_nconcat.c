f6e74937cd871fb1c773e8d989bbfbd8d5596848
RockoGilbert <evolve2122@gmail.com>
2021-07-26 15:49:45 -0500

    String Concat

--- 0x0E-more_malloc_free/1-string_nconcat.c
+++ 0x0E-more_malloc_free/1-string_nconcat.c
@@ -18,24 +18,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 	for (x1 = 0; s1[x1] != '\0'; x1++)
+
 	;
 
 	for (x2 = 0; s2[x2] != '\0'; x2++)
+
 	;
 
 	if (n >= x2)
 		n = x2;
-		s = malloc(sizeof(char) * (x1 + n + 1));
-
+	s = malloc(sizeof(char) * (x1 + n + 1));
 	if (s == NULL)
 		return (NULL);
-
 	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
 
 	for (j = x1; j < x1 + n; j++)
 		s[j] = s2[j - x1];
-		s[j] = '\0';
-
-		return (s);
+	s[j] = '\0';
+	return (s);
 }
