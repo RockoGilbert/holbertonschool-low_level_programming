17a6cdce4320d47749bdce0272255ff8e0256cac
RockoGilbert <evolve2122@gmail.com>
2021-07-26 12:26:59 -0500

    Created a struct for a dog

--- 0x0F-structures_typedef/dog.h
+++ 0x0F-structures_typedef/dog.h
@@ -9,11 +9,22 @@
  * Description: a new type that contains dog name, age, and owner.
  */
 
-type def struct dog
+typedef struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
 
 };
+
+/**
+ * dog _t - Typedef for dog structure
+ */
+typedefstruct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
