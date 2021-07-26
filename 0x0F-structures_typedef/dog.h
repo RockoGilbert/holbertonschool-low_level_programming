924c89b342480d55da425246acc6f4f08d8347e1
RockoGilbert <evolve2122@gmail.com>
2021-07-26 11:25:11 -0500

    Created README and header file

---
+++ 0x0F-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new structure
+ * @name : char
+ * @age : float
+ * @owner : char
+ * Description: a new type that contains dog name, age, and owner.
+ */
+
+type def struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+
+};
+#endif
