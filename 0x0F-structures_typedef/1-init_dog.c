92256ada980a2533d8348a727c26bf6714b7ba7b
RockoGilbert <evolve2122@gmail.com>
2021-07-26 20:45:28 -0500

    Wrote a function that initializes the variable

---
+++ 0x0F-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - intializes variable dog.
+ * @d: pointer to a struct dog
+ * @name: pointer to a string
+ * @age: float
+ * @owner: pointer to a string
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+
+	}
+
+}
