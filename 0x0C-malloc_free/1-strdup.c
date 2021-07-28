408adda77f3e6858013d3127dfde6dc8f77f8b65
RockoGilbert <evolve2122@gmail.com>
2021-07-28 15:46:39 -0500

    Fixed betty

--- 0x0C-malloc_free/1-strdup.c
+++ 0x0C-malloc_free/1-strdup.c
@@ -18,13 +18,13 @@ char *_strdup(char *str)
 		for (i = 0; str[i]; i++)
 			l++;
 		s = malloc(sizeof(char) * (l + 1));
-	
+
 		if (s == NULL)
 			return (NULL);
 
 		for (i = 0; str[i]; i++)
 			s[i] = str[i];
-		
+
 		s[l] = '\0';
 
 			return (s);
