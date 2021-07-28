6417f940322e9813df836d65b3e16258f1da1530
RockoGilbert <evolve2122@gmail.com>
2021-07-27 23:23:18 -0500

    added a prototype

--- 0x10-function_pointers/function_pointers.h
+++ 0x10-function_pointers/function_pointers.h
@@ -1,23 +1,11 @@
+#ifndef _funky
+#define _funky
+
+#include <stddef.h>
+
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
 
-#include <unistd.h>
-
-/**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
+#endif
