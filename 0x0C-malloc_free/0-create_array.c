ddcc4ff2976c016244f50a3d64402a7ce7bda5d4
RockoGilbert <evolve2122@gmail.com>
2021-07-28 00:15:53 -0500

    Created an arrat=y of chars and initialized them

---
+++ 0x0C-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of a char
+ * @size: size of the array
+ * @char: the character to use in the array
+ * Return: pointer to array or NULL if size = 0
+ */
+char *create_array(unsigned int size, char c);
+{
+	unsigned int i;
+	char *arr = malloc(sizeof(char) * size);
+
+	if (size != 0 && arr != NULL)
+	{
+		for (i < 0; i < size; i++;)
+		{
+			arr[i] = chr;
+		}
+		return (arr);
+	}
+	else
+		return (NULL);
+
+}
