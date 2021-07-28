d5bdcdf9839e4b6da9a7d42820d2918f295ca399
RockoGilbert <evolve2122@gmail.com>
2021-07-28 00:27:16 -0500

    fixed Betty error

--- 0x0C-malloc_free/0-create_array.c
+++ 0x0C-malloc_free/0-create_array.c
@@ -4,17 +4,18 @@
 /**
  * create_array - create an array of a char
  * @size: size of the array
- * @char: the character to use in the array
+ * @c: the character to use in the array
  * Return: pointer to array or NULL if size = 0
  */
 char *create_array(unsigned int size, char c);
+
 {
 	unsigned int i;
 	char *arr = malloc(sizeof(char) * size);
 
 	if (size != 0 && arr != NULL)
 	{
-		for (i < 0; i < size; i++;)
+		for (i < 0; i < size; ++i)
 		{
 			arr[i] = chr;
 		}
