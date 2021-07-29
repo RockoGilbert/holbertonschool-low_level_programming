ff97856fc838b4e178d4a7c02c37286fd698e2bd
RockoGilbert <evolve2122@gmail.com>
2021-07-29 12:06:03 -0500

    Variadic functions

---
+++ 0x11-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - gets sum of variadic function
+ * @n: num of args
+ * Return:int
+ */
+
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+unsigned int j;
+va_list ap;
+int sum;
+
+va_start(ap, n);
+
+if (n == 0)
+	return (0);
+for (j = 0; j < n; j++)
+	sum += va_arg(ap, int);
+
+va_end(ap);
+return (sum);
+
+}
