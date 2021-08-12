fbb4abb92901c42be6d6999029b1a82446842d94
RockoGilbert <evolve2122@gmail.com>
2021-08-11 22:03:51 -0500

    Returned the number of elements in a link list

--- 0x14-more_singly_linked_lists/1-listint_len.c
+++ 0x14-more_singly_linked_lists/1-listint_len.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 /**
- * add_nodeint - adds a node to the head of a list
- * @head: start of the list
- * @n: data to be added to the node
- *
- * Return: address of new node if success, NULL if fail
+ * listint_len - counts the number of notes in a linked list of ints
+ * @h: first node of list
+ * Return: number of nodes
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+size_t listint_len(const listint_t *h)
 {
-	listint_t *new_node;
+	int count;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	for (count = 0; h != NULL; count++)
+	{
+		if (h->n == '\0')
+			return (0);
+		h = h->next;
+	}
+	return (count);
 }
-
