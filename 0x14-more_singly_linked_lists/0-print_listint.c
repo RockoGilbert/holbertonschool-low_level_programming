d0c59dbc2472b001affe95287492054cbf55bead
RockoGilbert <evolve2122@gmail.com>
2021-08-11 20:44:06 -0500

    print the elements of a list

---
+++ 0x14-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,37 @@
+#include <lists.h>
+#include <stdio.h>
+
+/*
+ * print_listint - Prints the elements of a list
+ * @h: The head of the link list 
+ * Return: Number of nodes
+ */
+
+size_t print_listint(const listint_t *h) 
+{	
+	int count = 0;
+	int count;
+
+	if (h != NULL)
+	{
+	
+		while(h)
+		{
+		
+		printf("%d/n", h->n)
+		
+		
+		}		
+		
+		
+		
+		
+		
+	}
+
+
+
+
+
+
+}
