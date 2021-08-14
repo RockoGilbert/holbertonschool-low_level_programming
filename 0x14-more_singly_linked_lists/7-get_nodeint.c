eb9541f0d20bfe4adfaf0b7c504c0d96dfbeacbb
RockoGilbert <evolve2122@gmail.com>
2021-08-13 19:13:35 -0500

    fixed syntax error

--- 0x14-more_singly_linked_lists/7-get_nodeint.c
+++ 0x14-more_singly_linked_lists/7-get_nodeint.c
@@ -1,27 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+
 /**
- * get_nodeint_at_index - gets the data at node index
- * @head: first node of list
- * @index: location of data to find where index starts at 0
- * Return: data found
+ * get_nodeint_at_index - return nth node index
+ * @head:pointer to node
+ * @index:given index int
+ * Return:Null or index of node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
-	listint_t *current;
+size_t x;
 
-	if (head == NULL)
-		return (NULL);
-
-	current = head;
-	while (current != NULL)
-	{
-		if (i == index)
-			return (current);
-		i++;
-		current = current->next;
-	}
-	return (current);
+for (x = 0; x < index && head->next; x++)
+{
+	head = head->next;
+}
+if (x < index)
+	return (NULL);
+return (head);
 }
