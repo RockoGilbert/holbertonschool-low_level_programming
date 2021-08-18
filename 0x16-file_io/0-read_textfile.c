c0c7224c43173b5c8367bdb685e9827477e307e3
RockoGilbert <evolve2122@gmail.com>
2021-08-18 11:31:20 -0500

    git add 0-read_textfile.c !

--- 0x16-file_io/0-read_textfile.c
+++ 0x16-file_io/0-read_textfile.c
@@ -1,42 +1,39 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <unistd.h>
+#include "holberton.h"
+
 /**
- * read_textfile - reads a file and outputs to standard output
- * @filename: name of the file to be read
- * @letters: number of letters that it could read and print
- * Return: number of letters read and printed if success, 0 if failed
+ * read_textfile - read text and print to POSIX stdoutput
+ * @filename: name of file
+ * @letters:number of letters to read size_t
+ * Return: read the file display it or error 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buffer;
-	int write_test;
-	int count;
+	int fd, rd, wd;
+	char *buff;
 
-	if (filename == NULL)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buffer = malloc(letters);
-	if (buffer == NULL)
-	{
-		count = 0;
-		return (count);
-	}
-	count = read(fd, buffer, letters);
-	write_test = write(STDOUT_FILENO, buffer, count);
-	if (write_test == -1)
-	{
-		count = 0;
-		return (count);
-	}
-	free(buffer);
+	fd = open(filename,  O_RDONLY);
+		if (fd <= -1)
+		{
+			return (0);
+		}
+	buff = malloc(sizeof(char) * letters);
+		if (buff == NULL)
+			return (0);
+
+	rd = read(fd, buff, letters);
+		if (rd <= -1)
+		{
+			return (0);
+		}
+	wd = write(STDOUT_FILENO, buff, rd);
+		if (wd <= -1)
+		{
+			return (0);
+		}
+
 	close(fd);
-	return (count);
+	free(buff);
+	return (wd);
+
+
 }
