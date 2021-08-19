27ea3cb2af340ebb858066dd6d3e01c6b39fe049
RockoGilbert <evolve2122@gmail.com>
2021-08-19 12:28:55 -0500

    Added a node

--- 0x14-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x14-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,46 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
-
 /**
- * insert_nodeint_at_index - inserts a new node at a given index
- * @head: a pointer to list
- * @index: the given index
- * @n: the given number
- * Return: a pointer to the newly add node
+ * insert_nodeint_at_index - adds a node to a linked list at a specific point
+ * @head: start of list
+ * @idx: index where node should be added
+ * @n: data to add to node
+ * Return: address of node if success, NULL if fail
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *t;
+	listint_t *current;
+	listint_t *new_node;
 	unsigned int i = 0;
 
-	new = malloc(sizeof(listint_t));
-	if (!new)
+	if (!head || (*head == NULL && idx > 0))
 		return (NULL);
-	new->n = n;
-
-	if (*head == NULL)
-	{
-		*head = new;
-		new->next = NULL;
-		return (new);
-	}
-	if (index == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
-
-	t = *head;
-	while (i < (index - 1) && t->next)
-	{
-		t = t->next;
-		i++;
-	}
-	if (i < index - 1)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
-
-	new->next = t->next;
-	t->next = new;
-
-	return (new);
+	if (idx == 0)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+	else
+	{
+		current = *head;
+		for (i = 1; current && i < idx; i++)
+			current = current->next;
+		if (i != idx)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		else
+		{
+			new_node->n = n;
+			new_node->next = current->next;
+			current->next = new_node;
+		}
+																}
+	return (new_node);
 }
