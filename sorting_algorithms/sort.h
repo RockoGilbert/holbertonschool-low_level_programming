443e0b079aaa4283f0161da3b98155cc4ac584ba
RockoGilbert <evolve2122@gmail.com>
2021-09-19 21:13:25 -0500

    edited prototype

--- sorting_algorithms/sort.h
+++ sorting_algorithms/sort.h
@@ -1,6 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  * @n: Integer stored in the node
@@ -22,5 +25,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap_nodes(listint_t *l);
 
 #endif
